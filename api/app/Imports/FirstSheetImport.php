7e181856cc0421a852fe88578d224b357e2c38fb
Sagar Shourov Roy <sagarshourov@gmail.com>
2023-02-10 11:25:13 +0600

    format

--- api/app/Imports/FirstSheetImport.php
+++ api/app/Imports/FirstSheetImport.php
@@ -1,5 +1,7 @@
-<?php 
+<?php
+
 namespace App\Imports;
+
 use Maatwebsite\Excel\Concerns\SkipsUnknownSheets;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Illuminate\Support\Collection;
@@ -17,16 +19,17 @@ class FirstSheetImport implements ToCollection
     private $user_id;
     private $file_name;
 
-    public function __construct( int $user_id, string $file_name)
+    public function __construct(int $user_id, string $file_name)
     {
         $this->user_id = $user_id;
-   
+
         $this->file_name = $file_name;
     }
-    private $sections=null;
+    private $sections = null;
 
-    private function check($title){
-      $pkg =  Sections::firstOrCreate([
+    private function check($title)
+    {
+        $pkg =  Sections::firstOrCreate([
             'title' => $title
         ]);
 
@@ -38,53 +41,51 @@ class FirstSheetImport implements ToCollection
 
     public function collection(Collection $rows)
     {
-//         $package = Package::get()->pluck('title')->toArray();
-// print_r($package);
+        //         $package = Package::get()->pluck('title')->toArray();
+        // print_r($package);
 
-        foreach ($rows as $row) 
-        {
-            if($row[0]=='First Name'){
-              continue;
+        foreach ($rows as $row) {
+            if ($row[0] == 'First Name') {
+                continue;
             }
 
-            if(!empty($row[1])){
-            //     print_r($row[0]);
-            //   // print_r($row);
-            //    echo '<br/>';
-               
-            //    echo $this->sections;
-            $in['first_name'] = $row[0];
-            $in['last_name']=$row[1];
-            $in['phone_number']=$row[2];
-            $in['email']=$row[3];
-             
-            $in['last_status_notes']=$row[4];
+            if (!empty($row[1])) {
+                //     print_r($row[0]);
+                //   // print_r($row);
+                //    echo '<br/>';
 
-            $in['status']=Status::firstOrCreate([
-                'title' => $row[5]
-            ])->id;; //db
-            $in['ag']=$row[6]; //db
+                //    echo $this->sections;
+                $in['first_name'] = $row[0];
+                $in['last_name'] = $row[1];
+                $in['phone_number'] = $row[2];
+                $in['email'] = $row[3];
 
-            $in['package'] = Package::firstOrCreate([
-                'title' => $row[7]
-            ])->id; //db
-            $in['age']=$row[9]; //db
-            $in['follow_up_notes']=$row[10];
-            $in['sections']=$this->sections;
-            $in['user_id']=$this->user_id;
-            $in['file_name']=$this->file_name;
-            Calls::create($in);            
-           // echo '<br/>';
+                $in['last_status_notes'] = $row[4];
+
+                $in['status'] = Status::firstOrCreate([
+                    'title' => $row[5]
+                ])->id;; //db
+                $in['ag'] = $row[6]; //db
+
+                $in['package'] = Package::firstOrCreate([
+                    'title' => $row[7]
+                ])->id; //db
+                $in['age'] = $row[9]; //db
+                $in['follow_up_notes'] = $row[10];
+                $in['sections'] = $this->sections;
+                $in['user_id'] = $this->user_id;
+                $in['file_name'] = $this->file_name;
+                Calls::create($in);
+                // echo '<br/>';
                 //echo '______END///////////////________';
-            }else if(!empty($row[0])){
+            } else if (!empty($row[0])) {
                 // print_r($row[0]);
                 // echo '<br/>';
                 // print_r($row[1]);
-                $this->sections=$this->check($row[0]);
+                $this->sections = $this->check($row[0]);
             }
-        } 
+        }
         // echo '______END///////////////________';
         // echo '<br/>';
     }
-    
 }
